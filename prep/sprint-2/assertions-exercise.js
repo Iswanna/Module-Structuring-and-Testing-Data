fce4c591b8e9ce8c6221547ff799b992cb063314
iswat <ibeetek@gmail.com>
2025-10-11 15:04:25 +0100

    Add assertion to verify 24-hour to 12-hour conversion for 23:00 → 11:00 pm

--- prep/sprint-2/assertions-exercise.js
+++ prep/sprint-2/assertions-exercise.js
@@ -17,9 +17,10 @@ console.assert(
   `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
-const currentOutput1 = formatAs12HourClock("23:00");
-const targetOutput1 = "11:00 pm";
+
+const currentOutput2 = formatAs12HourClock("23:00");
+const targetOutput2 = "11:00 pm";
 console.assert(
-  currentOutput === targetOutput,
-  `current output: ${currentOutput}, target output: ${targetOutput}`
+  currentOutput2 === targetOutput2,
+  `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
