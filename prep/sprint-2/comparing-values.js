b968481f8458f8fcc57aafeabd7503d12d27fc38
iswat <ibeetek@gmail.com>
2025-10-11 16:26:03 +0100

    Implement function to compare two arguments numerically
    - Converts both arguments from strings to numbers before comparison

--- prep/sprint-2/comparing-values.js
+++ prep/sprint-2/comparing-values.js
@@ -1,5 +1,18 @@
-function formatAs12HourClock() {
+//function formatAs12HourClock() {
 
+//}
+
+//console.log(formatAs12HourClock("08:00") === "08:00am")
+
+function compareTwoNumbers (a , b) {
+    if (a > b) {
+        return `${a} is greater than ${b};`
+    }
+    else if (a === b) {
+        return `${a} is equals to ${b}`;
+    }
+    return `${b} is greater than ${a}`;
 }
 
-console.log(formatAs12HourClock("08:00") === "08:00am")
+const compareResult = compareTwoNumbers(Number("100") , Number("100"));
+console.log(compareResult);
